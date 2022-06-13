937d1879aee6958a993073077f2487ea715fd4a3
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-13 19:14:06 +0900

    [2022 kakao] 문제 1

--- 2022_kakao/1.cpp
+++ 2022_kakao/1.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
@@ -13,7 +12,8 @@ struct user_log
 	int reported_count = 0;
 };
 
-vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+vector<int> solution(vector<string> id_list, vector<string> report, int k)
+{
     vector<int> answer;
 	map<string, struct user_log> users;
     
@@ -30,7 +30,6 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     
 	for (int i = 0; i < report.size(); i++)
 	{
-        cout << report[i] << "\n";
 		string reporter = report[i].substr(0, report[i].find(' '));
 		string reported = report[i].substr(report[i].find(' ') + 1);
 		users[reported].reported_user.push_back(reporter);
@@ -40,12 +39,10 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
 	for (int i = 0; i < id_list.size(); i++)
 	{
 		struct user_log log = users[id_list[i]];
-        cout << log.id << ": " << log.reported_count << "\n";
 		if (log.reported_count >= k)
 		{
 			for (int j = 0; j < log.reported_user.size(); j++)
 			{
-                cout << ">> " <<log.reported_user[j] << ": " << users[log.reported_user[j]].id << "\n";
                 if (users[log.reported_user[j]].id == -1)
                     continue;
 				answer[users[log.reported_user[j]].id]++;
