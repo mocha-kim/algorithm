d5c271fb5d213d3d2101ba7d0c5ca75160fa0cd8
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-16 18:02:14 +0900

    [hash] 42596

---
+++ programmers/hash/42596.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    
+    unordered_map<string, int> map;
+    
+    for (string runner : participant)
+        map[runner]++;
+    
+    for (string runner : completion)
+        map[runner]--;
+    
+    for (auto tmp : map)
+    {
+        if (tmp.second == 1)
+            return tmp.first;
+    }
+}
