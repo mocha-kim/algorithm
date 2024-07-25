653cef3653806f1364cafa2c018801e273081349
mocha-kim <dev.sunhye@gmail.com>
2024-07-25 23:41:41 +0900

    [boj] 12865 평범한 배낭

--- boj/gold/boj_12865.cpp
+++ boj/gold/boj_12865.cpp
@@ -6,21 +6,19 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    int w, v;
-    vector<pair<int, int>> objects(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> w >> v;
-        objects[i] = { w, v };
-    }
+    vector<int> weights(n + 1);
+    vector<int> values(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> weights[i] >> values[i];
 
     vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= k; j++) {
-            if (objects[i].first < j) {
+            if (weights[i] > j) {
                 dp[i][j] = dp[i - 1][j];
                 continue;
             }
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - objects[i].first] + objects[i].second);
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
         }
     }
     cout << dp[n][k];
