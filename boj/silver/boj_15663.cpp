f04e09dd9502c1582ce06339aa03bd976ddc4526
mocha-kim <dev.sunhye@gmail.com>
2024-07-26 22:03:08 +0900

    [boj] 15663 N과 M (9)

---
+++ boj/silver/boj_15663.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int g_n, g_m;
+int seq[8];
+
+void dfs(int count, vector<int>& numbers, vector<bool>& is_visited);
+
+int main() {
+    cin >> g_n >> g_m;
+    vector<int> numbers(g_n);
+    for (int i = 0; i < g_n; i++)
+        cin >> numbers[i];
+    sort(numbers.begin(), numbers.end());
+
+    vector<bool> is_visited(g_n, false);
+    dfs(0, numbers, is_visited);
+    return 0;
+}
+
+void dfs(int count, vector<int>& numbers, vector<bool>& is_visited) {
+    if (count == g_m) {
+        for (int i = 0; i < g_m; i++)
+            cout << seq[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int last = 0;
+    for (int i = 0; i < g_n; i++) {
+        if (is_visited[i] || numbers[i] == last)
+            continue;
+        last = numbers[i];
+        seq[count] = numbers[i];
+        is_visited[i] = true;
+        dfs(count + 1, numbers, is_visited);
+        is_visited[i] = false;
+    }
+}
